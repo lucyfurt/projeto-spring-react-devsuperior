142a3db219154146a1aa1e2f456a4dfc259a31dc
lucyfurt <lucianafurtado10@gmail.com>
2021-05-15 19:05:27 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -23,9 +23,11 @@ const Dashboard = () => {
 
         <div className="py-3">
           <h2 className="text-primary">Todas Vendas</h2>
+        
         </div>
         <DataTable />
       </div>
+      
       <Footer />
     </>
   );
